9921737df9c4606da2c961491dc83320c39941f2
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-06 03:17:29 +0000

    added the front page skeleton and placeholder for components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="AppContainer">
+        <section className="React">React</section>
+        <section className="CSS">HTML/CSS</section>
+        <section className="Node">Node</section>
+        <section className="ES6">ES6</section>
       </div>
     );
   }
