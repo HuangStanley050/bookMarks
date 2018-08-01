31a477539a4450eaf5403a55d5f11e8d80634f54
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-01 06:27:55 +0000

    added the signin auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const auth_success = (authData) => {
     return {
@@ -23,7 +24,21 @@ export const auth_fail = (error) => {
 };
 
 export const auth = (email, password) => {
+    const key = "AIzaSyBxt6vwGXw_G7nSngDz7J1Beb2XTUAfCy4";
+    const authData = {
+        email: email,
+        password: password,
+        returnSecureToken: true
+    };
     return dispatch => {
         dispatch(auth_start());
+        axios.post(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${key}`, authData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(auth_fail(err));
+            });
     };
 };
